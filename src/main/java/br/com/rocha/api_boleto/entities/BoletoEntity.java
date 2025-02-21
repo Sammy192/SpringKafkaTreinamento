70575713c5134fc8c9d4c435c0c7509f498b4743
Samuel Rodrigues Borges <samuel.borges@e-unicred.com.br>
2025-02-21 11:58:57 -0300

    Cria Entidade boletoEntity

---
+++ src/main/java/br/com/rocha/api_boleto/entities/BoletoEntity.java
@@ -0,0 +1,31 @@
+package br.com.rocha.api_boleto.entities;
+
+import br.com.rocha.api_boleto.entities.enums.SituacaoBoletoEnum;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BoletoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String codigoBarras;
+    @Column
+    private SituacaoBoletoEnum situacaoBoleto;
+    @Column
+    private LocalDateTime dataCriacao;
+    @Column
+    private LocalDateTime dataAtualizacao;
+}
---
+++ src/main/java/br/com/rocha/api_boleto/entities/enums/SituacaoBoletoEnum.java
@@ -0,0 +1,10 @@
+package br.com.rocha.api_boleto.entities.enums;
+
+public enum SituacaoBoletoEnum {
+
+    INICIALIZADO,
+    VALIDADO,
+    ERRO_VALIDACAO,
+    PAGO,
+    ERRO_PAGAMENTO,
+}
