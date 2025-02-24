da4215330697e9fc74aa734d8f8bda2b44a295e2
Samuel Rodrigues Borges <samuel.borges@e-unicred.com.br>
2025-02-24 10:17:03 -0300

    Remove leitura de apenas mensagens novas

--- validador-boleto/src/main/java/br/com/rocha/validador_boleto/services/kafka/BoletoConsumer.java
+++ validador-boleto/src/main/java/br/com/rocha/validador_boleto/services/kafka/BoletoConsumer.java
@@ -1,17 +1,13 @@
 package br.com.rocha.validador_boleto.services.kafka;
 
 import br.com.rocha.avro.Boleto;
-import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
-public class BoletoConsumer implements ConsumerSeekAware {
+public class BoletoConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BoletoConsumer.class);
 
@@ -19,11 +15,5 @@ public class BoletoConsumer implements ConsumerSeekAware {
     public void consomeBoleto(Boleto boleto) throws InterruptedException {
         Thread.sleep(3000);
         LOGGER.info("Novo boleto recebido: {}", boleto);
-    }
-
-    @Override
-    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
-        //ve apenas mensagens novas
-        assignments.forEach((t, o) -> callback.seekToEnd(t.topic(), t.partition()));
     }
 }
