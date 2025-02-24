147becb80076a612d4546109dc16b4b963d07d20
Samuel Rodrigues Borges <samuel.borges@e-unicred.com.br>
2025-02-24 10:11:27 -0300

    Configura offset e leitura apenas de novas mensagens

--- apiBoleto/src/main/java/br/com/rocha/apiboleto/services/kafka/BoletoProducer.java
+++ apiBoleto/src/main/java/br/com/rocha/apiboleto/services/kafka/BoletoProducer.java
@@ -17,6 +17,13 @@ public class BoletoProducer {
     }
 
     public void enviarMensagem(Boleto boleto) {
-        kafkaTemplate.send(topico, boleto);
+        kafkaTemplate.send(topico, getKey(boleto), boleto);
+    }
+
+    private String getKey(Boleto boleto) {
+        if (boleto.getCodigoBarras().toString().substring(0,1).equals("2")) {
+            return "chave1";
+        }
+        return "chave2";
     }
 }
--- validador-boleto/src/main/java/br/com/rocha/validador_boleto/services/kafka/BoletoConsumer.java
+++ validador-boleto/src/main/java/br/com/rocha/validador_boleto/services/kafka/BoletoConsumer.java
@@ -1,18 +1,29 @@
 package br.com.rocha.validador_boleto.services.kafka;
 
 import br.com.rocha.avro.Boleto;
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
-public class BoletoConsumer {
+public class BoletoConsumer implements ConsumerSeekAware {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BoletoConsumer.class);
 
     @KafkaListener(topics = "${spring.kafka.topico-boleto}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consomeBoleto(Boleto boleto) {
+    public void consomeBoleto(Boleto boleto) throws InterruptedException {
+        Thread.sleep(3000);
         LOGGER.info("Novo boleto recebido: {}", boleto);
+    }
+
+    @Override
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+        //ve apenas mensagens novas
+        assignments.forEach((t, o) -> callback.seekToEnd(t.topic(), t.partition()));
     }
 }
